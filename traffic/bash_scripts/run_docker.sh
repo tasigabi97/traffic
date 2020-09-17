85e620bf7f2c4a78630154d8f6d509bb90b45a92
Tasnádi Gábor <tasi.gabi97@gmail.com>
2020-09-17 20:00:44 +0200

    save

--- traffic/bash_scripts/run_docker.sh
+++ traffic/bash_scripts/run_docker.sh
@@ -1,4 +1,10 @@
+echo 1.15.2 working
+echo 1.12.0 not working
+sudo docker run --runtime=nvidia --gpus all      -t -it --rm --ipc=host -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
+echo 1.12.0 working
 sudo docker run --runtime=nvidia --gpus all -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_REQUIRE_CUDA="cuda>=10.0 brand=tesla,driver>=384,driver<385 brand=tesla,driver>=410,driver<411"    -t -it --rm --ipc=host -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
+
+
 #    python /traffic/traffic/examples/tensorflow_1.py
 #   -e az environment
 #    echo $NVIDIA_REQUIRE_CUDA
