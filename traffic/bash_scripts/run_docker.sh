6231a02a896b5c5210f3da3a512e1bb7717caa3a
Tasnádi Gábor <tasi.gabi97@gmail.com>
2020-09-17 23:05:29 +0200

    wifi dockerben

--- traffic/bash_scripts/run_docker.sh
+++ traffic/bash_scripts/run_docker.sh
@@ -1,11 +1,17 @@
-echo 1.15.2 working
-echo 1.12.0 not working
-sudo docker run --runtime=nvidia --gpus all      -t -it --rm --ipc=host -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
+# hosszú
+#sudo docker run -d=false -it --privileged --net=host --ipc=host --runtime=nvidia --gpus all -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_REQUIRE_CUDA="cuda>=10.0 brand=tesla,driver>=384,driver<385 brand=tesla,driver>=410,driver<411"   -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
 echo 1.12.0 working
-sudo docker run --runtime=nvidia --gpus all -e NVIDIA_VISIBLE_DEVICES=all -e NVIDIA_DRIVER_CAPABILITIES=all -e NVIDIA_REQUIRE_CUDA="cuda>=10.0 brand=tesla,driver>=384,driver<385 brand=tesla,driver>=410,driver<411"    -t -it --rm --ipc=host -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
+sudo docker run -it --privileged --net=host --ipc=host --gpus all -v "/home/gabi/PycharmProjects/traffic/:/traffic" tensorflow/tensorflow:1.12.0-gpu-py3 python /traffic/traffic/examples/tensorflow_1.py
 
 
-#    python /traffic/traffic/examples/tensorflow_1.py
-#   -e az environment
-#    echo $NVIDIA_REQUIRE_CUDA
-#   -e NVIDIA_REQUIRE_CUDA="cuda>=10.0 brand=tesla,driver>=384,driver<385 brand=tesla,driver>=410,driver<411"
+
+# --rm=true # kilépéskor törli a fájlokat, de nem működik eddig
+# --privileged #  bekapcsol minden eszközt
+# -it # -t -i elvileg
+# --gpus all # --runtime=nvidia # videókártya támogatás, elvileg mindkettő ugyanaz de az első újabb
+# -e # környezeti változó beállítás
+# -v "/home/gabi/PycharmProjects/traffic/:/traffic" # eléri a mappát a konténer
+# --ipc=host # nem tűnik fontosnak eddig
+# -d=false # előtérben fut
+
+
