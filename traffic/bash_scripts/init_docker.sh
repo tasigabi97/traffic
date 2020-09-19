9e26f11803c77fa2be435a7928fad30bcb59a3fd
Tasnádi Gábor <tasi.gabi97@gmail.com>
2020-09-19 23:30:01 +0200

    tf+gui 3.

--- traffic/bash_scripts/init_docker.sh
+++ traffic/bash_scripts/init_docker.sh
@@ -4,6 +4,10 @@ apt install -y wireless-tools
 apt install -y python-opencv
 pip install --upgrade pip
 pip install opencv-python
+pip install cython
+pip install scikit-image
+pip install imgaug
+pip install IPython[all]
 # beállítja az ezt beolvasó bash aliasát
 shopt -s expand_aliases
 alias python=python3
